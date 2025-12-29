2b1460092acef4e9007410d090ce78d1d220e8ea
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2025-12-29 18:29:58 +0100

    Add Reiziger class

---
+++ src/reiziger/Reiziger.java
@@ -0,0 +1,79 @@
+package reiziger;
+
+import java.time.LocalDate;
+
+public class Reiziger {
+    private Integer reiziger_id;
+    private String voorletters;
+    private String tussenvoegsel;
+    private String achternaam;
+    private LocalDate geboortedatum;
+
+    public Reiziger(Integer reiziger_id, String voorletters, String tussenvoegsel, String achternaam, LocalDate geboortedatum) {
+        this.reiziger_id = reiziger_id;
+        this.voorletters = voorletters;
+        this.tussenvoegsel = tussenvoegsel;
+        this.achternaam = achternaam;
+        this.geboortedatum = geboortedatum;
+    }
+
+    public String getNaam() {
+        String naam = "";
+        naam += voorletters + ". ";
+        if (tussenvoegsel != null) {
+            naam += tussenvoegsel + " ";
+        }
+        naam += achternaam;
+        return naam;
+    }
+
+    public String toString() {
+        return "#%d %s (%s)".formatted(
+            reiziger_id,
+            this.getNaam(),
+            geboortedatum
+        );
+    }
+
+    //*** Get/Set ***//
+
+    public Integer getReiziger_id() {
+        return reiziger_id;
+    }
+
+    public void setReiziger_id(Integer reiziger_id) {
+        this.reiziger_id = reiziger_id;
+    }
+
+    public String getVoorletters() {
+        return voorletters;
+    }
+
+    public void setVoorletters(String voorletters) {
+        this.voorletters = voorletters;
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        this.tussenvoegsel = tussenvoegsel;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public LocalDate getGeboortedatum() {
+        return geboortedatum;
+    }
+
+    public void setGeboortedatum(LocalDate geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+}
